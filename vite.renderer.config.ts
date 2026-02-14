f99a4ebde889ab6ece035d9003a0e08ad3a0d840
Mohd Rejoan <wearecrx@gmail.com>
2026-02-14 11:21:35 +0600

    typescript module resolve

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [tailwindcss()],
+});
